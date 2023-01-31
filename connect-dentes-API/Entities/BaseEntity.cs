414da0de3c84c2a9187afca02f459081cb40eec0
Pedro Henrique Sousa Pires <pedropires2603@gmail.com>
2023-01-31 15:40:34 -0300

    Entidades básicas

---
+++ connect-dentes-API/Entities/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace connect_dentes_API.Entities
+{
+    public abstract class BaseEntity
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("data_cadastro")]
+        public DateTime DataCadastro { get; set; }
+
+        [Column("usuario_cadastro")]
+        public string? UsuarioCadastro { get; set; }
+
+        [Column("data_edicao")]
+        public string? DataEdicao { get; set; }
+
+        [Column("usuario_edicao")]
+        public string? UsuarioEdicao { get; set; }
+    }
+}
---
+++ connect-dentes-API/Entities/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using connect_dentes_API.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace connect_dentes_API.Entities
+{
+    [Table("Usuario")]
+    public class Usuario : BaseEntity
+    {
+        [Column("nome")]
+        public string Nome { get; set; }
+
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Column("ativo")]
+        public bool Ativo { get; set; }
+
+        [Column("senha")]
+        public string Senha { get; set; }
+
+        [Column("salt")]
+        public string? Salt { get; set; }
+
+        [Column("role")]
+        public Roles Role { get; set; }
+    }
+}
