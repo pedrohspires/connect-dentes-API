48c66ca1768167d9b42e2a2794669eea4f495e41
Pedro Henrique Sousa Pires <pedropires2603@gmail.com>
2023-01-31 15:41:39 -0300

    Serviço de autenticação

---
+++ connect-dentes-API/Services/Implementations/AuthService.cs
@@ -0,0 +1,71 @@
+﻿using connect_dentes_API.DTOs;
+using connect_dentes_API.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace connect_dentes_API.Services.Implementations
+{
+    public class AuthService : IAuthService
+    {
+        public readonly IConfiguration _configuration;
+
+        public AuthService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private string ByteToString(byte[] data)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            foreach (byte b in data)
+            {
+                builder.Append(b.ToString("x2"));
+            }
+
+            return builder.ToString();
+        }
+
+        public string GetHashSenhaSHA256(string senha, string? salt)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(senha + salt));
+
+                return ByteToString(bytes);
+            }
+        }
+
+        public string GetSalt()
+        {
+            var salt = new byte[32];
+            var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(salt);
+            return ByteToString(salt);
+        }
+
+        public string GetToken(UsuarioDto usuario)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, usuario.Nome),
+                new Claim(ClaimTypes.Role, usuario.Role.ToString())
+            };
+            var token = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddHours(4),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ connect-dentes-API/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using connect_dentes_API.DTOs;
+
+namespace connect_dentes_API.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        public string GetHashSenhaSHA256(string senha, string? salt);
+        public string GetSalt();
+        public string GetToken(UsuarioDto usuario);
+    }
+}
