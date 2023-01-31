962c82f0b6318500c79bab03adb17eb884b52850
Pedro Henrique Sousa Pires <pedropires2603@gmail.com>
2023-01-31 15:43:04 -0300

    Migrations de usuário

---
+++ connect-dentes-API/Migrations/20230131113101_Usuario.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace connectdentesAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Usuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    nome = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    ativo = table.Column<bool>(type: "boolean", nullable: false),
+                    senha = table.Column<string>(type: "text", nullable: false),
+                    salt = table.Column<string>(type: "text", nullable: true),
+                    role = table.Column<int>(type: "integer", nullable: false),
+                    datacadastro = table.Column<DateTime>(name: "data_cadastro", type: "timestamp without time zone", nullable: false),
+                    usuariocadastro = table.Column<string>(name: "usuario_cadastro", type: "text", nullable: true),
+                    dataedicao = table.Column<string>(name: "data_edicao", type: "text", nullable: true),
+                    usuarioedicao = table.Column<string>(name: "usuario_edicao", type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
