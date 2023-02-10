951c9c1e20bea63d1242c54d6b34b7f4c21b3524
Pedro Henrique Sousa Pires <pedropires2603@gmail.com>
2023-02-10 14:57:58 -0300

    Dtos para usuário

--- connect-dentes-API/DTOs/UsuarioCadastroDto.cs
+++ connect-dentes-API/DTOs/UsuarioCadastroDto.cs
@@ -5,6 +5,7 @@
         public string? Nome { get; set; }
         public string? Email { get; set; }
         public string? Senha { get; set; }
+        public string? Tipo { get; set; }
         public bool Ativo { get; set; }
     }
 }
