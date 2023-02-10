412a7cb5dc47f0f00b930a6367d3cf5b3a627f84
Pedro Henrique Sousa Pires <pedropires2603@gmail.com>
2023-02-10 14:58:57 -0300

    Nova entidade atendimento

---
+++ connect-dentes-API/Entities/Atendimento.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace connect_dentes_API.Entities
+{
+    [Table("Atendimento")]
+    public class Atendimento : BaseEntity
+    {
+        [Column("id_medico")]
+        public int MedicoId { get; set; }
+        public Usuario? Medico { get; set; }
+
+        [Column("id_cliente")]
+        public int ClienteId{ get; set; }
+        public Cliente? Cliente { get; set; }
+
+        [Column("detalhes")]
+        public string Detalhes { get; set; }
+
+        [Column("observacoes")]
+        public string? Observacoes { get; set; }
+
+        [Column("dentes")]
+        public string? Dentes { get; set; }
+
+        [Column("data_atendimento")]
+        public DateTime DataAtendimento { get; set; }
+
+        [Column("data_retorno")]
+        public DateTime? DataRetorno { get; set; }
+    }
+}
