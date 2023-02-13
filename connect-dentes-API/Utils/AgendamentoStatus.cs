12d0ce0bd93adb39421b196334913dbb12eb9f1b
Pedro Henrique Sousa Pires <pedropires2603@gmail.com>
2023-02-13 14:42:01 -0300

    Agendamento: controle de status

---
+++ connect-dentes-API/Utils/AgendamentoStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace connect_dentes_API.Utils
+{
+    public static class AgendamentoStatus
+    {
+        public static string Agendado { get => "AGENDADO"; }
+        public static string Atendido { get => "ATENDIDO"; }
+        public static string Atrasado { get => "ATRASADO"; }
+        public static string AtendidoComAtraso { get => "ATENDIDO COM ATRASO"; }
+    }
+}
