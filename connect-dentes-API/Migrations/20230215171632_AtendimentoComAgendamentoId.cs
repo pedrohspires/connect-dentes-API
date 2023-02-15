cebbf0f01f55ea704d2599a54c8678fd1cd70d9f
Pedro Henrique Sousa Pires <pedropires2603@gmail.com>
2023-02-15 16:43:58 -0300

    Novas migrations após alteração da entidade atendimento

---
+++ connect-dentes-API/Migrations/20230215171632_AtendimentoComAgendamentoId.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace connectdentesAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtendimentoComAgendamentoId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "id_agendamento",
+                table: "Atendimento",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Atendimento_id_agendamento",
+                table: "Atendimento",
+                column: "id_agendamento");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Atendimento_agendamento_id_agendamento",
+                table: "Atendimento",
+                column: "id_agendamento",
+                principalTable: "agendamento",
+                principalColumn: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Atendimento_agendamento_id_agendamento",
+                table: "Atendimento");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Atendimento_id_agendamento",
+                table: "Atendimento");
+
+            migrationBuilder.DropColumn(
+                name: "id_agendamento",
+                table: "Atendimento");
+        }
+    }
+}
